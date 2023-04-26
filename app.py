6d257a7d2f28f1b64fae6ff25558b1e1f34d2e1d
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-26 15:21:35 -0700

    implementing user page

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from models import connect_db, User, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +12,29 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get('/')
+def show_homepage():
+    """Display Home Page"""
+
+    return render_template("base.html")
+
+
+
+@app.post("/")
+def process_incoming_data():
+    """Process the post requests on homepage"""
+
+
+@app.get("/user")
+def show_users_page():
+    """Display user page"""
+
+    users = User.query.all()
+    return render_template("user.html", users = users)
+
+@app.get("/user/<int:id>")
+def show_user_profile(id):
+    """Display single user profile via user id"""
+
+    return  render_template("user_profile.html")
--- models.py
+++ models.py
@@ -1,1 +1,35 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """ Users"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(20),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(20),
+        nullable=False)
+
+    image_url = db.Column(
+        db.String(),
+        default="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80")
+
